5de8ae38b2eec961ed50b2f5598b029784ee6167
KelvinQiu <kelvinqiu802@outlook.com>
2022-08-23 15:28:59 +0800

    feaet: Mobile Adaptive

--- src/components/Home/Welcome.jsx
+++ src/components/Home/Welcome.jsx
@@ -1,19 +1,39 @@
-import { Stack, Typography, Button } from '@mui/material';
+import {
+  Stack,
+  Typography,
+  Button,
+  useTheme,
+  useMediaQuery,
+} from '@mui/material';
 import { useNavigate } from 'react-router-dom';
+import catImage from '../../images/home-cat.svg';
 
 const Welcome = () => {
   const navigate = useNavigate();
+  const theme = useTheme();
+  const mobile = useMediaQuery(theme.breakpoints.down('sm'));
+
   const handleClick = () => {
     navigate('/question');
   };
 
   return (
-    <Stack>
+    <Stack alignItems='center'>
       <Typography
         variant='h1'
-        sx={{ fontWeight: 400, mb: '-20px', fontStyle: 'italic' }}
+        sx={{
+          fontWeight: 400,
+          mb: mobile ? '-10px' : '-20px',
+          fontStyle: 'italic',
+          fontSize: mobile ? '3em' : '5.5em',
+        }}
       >
-        Choose <span style={{ fontStyle: 'normal' }}>🐈</span>
+        Choose{' '}
+        <span
+          style={{ fontStyle: 'normal', display: mobile ? 'none' : 'inline' }}
+        >
+          🐈
+        </span>
       </Typography>
       <Typography
         variant='h1'
@@ -21,6 +41,7 @@ const Welcome = () => {
           fontWeight: 500,
           fontStyle: 'italic',
           mb: 1.8,
+          fontSize: mobile ? '4em' : '6.9em',
         }}
       >
         Your <span style={{ color: '#FEA130' }}>Cat</span>
@@ -28,24 +49,32 @@ const Welcome = () => {
       <Typography
         variant='p'
         color='text.secondary'
-        sx={{ width: '400px', ml: 1.5, fontSize: '1.2em' }}
+        sx={{
+          width: mobile ? '300px' : '400px',
+          ml: 1.5,
+          fontSize: mobile ? '0.9em' : '1.2em',
+        }}
       >
         This site will help you to choose the best breed of cat for you{' '}
         <strong>
           <i>by asking you a few questions.</i>
         </strong>
       </Typography>
+      {mobile && (
+        <img src={catImage} width='300px' style={{ marginTop: '10px' }} />
+      )}
       <Button
         variant='outlined'
         sx={{
           borderRadius: 5,
-          mt: 3,
+          mt: mobile ? 2 : 3,
           fontSize: '1.2em',
           fontWeight: 500,
         }}
         color='warning'
         size='big'
         onClick={handleClick}
+        fullWidth
       >
         Let's Start!
       </Button>
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,20 +1,36 @@
-import { Box, Typography, Stack, Tooltip, Button } from '@mui/material';
+import {
+  Box,
+  Typography,
+  Stack,
+  Tooltip,
+  Button,
+  useTheme,
+  useMediaQuery,
+} from '@mui/material';
 import catImg from '../images/home-cat.svg';
 import Welcome from '../components/Home/Welcome';
 
 const Home = () => {
+  const theme = useTheme();
+  const mobile = useMediaQuery(theme.breakpoints.down('sm'));
+
   return (
-    <Box className='wrapper'>
+    <Box className={mobile ? '' : 'wrapper'}>
       <Stack
         direction='row'
         justifyContent='space-evenly'
         alignItems='center'
-        className='content'
+        className={mobile ? '' : 'content'}
       >
         <Welcome />
         <Tooltip title='@designed by Freepik' placement='top-start'>
           <a href='https://www.freepik.com' target='_blank'>
-            <img src={catImg} alt='Cat Image' width='600px' />
+            <img
+              src={catImg}
+              alt='Cat Image'
+              width='600px'
+              style={{ display: mobile ? 'none' : 'flex' }}
+            />
           </a>
         </Tooltip>
       </Stack>
