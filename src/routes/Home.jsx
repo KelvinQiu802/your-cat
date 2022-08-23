c3355b5fa378d89869b0ae020f3c83cbab9536fe
KelvinQiu <kelvinqiu802@outlook.com>
2022-08-23 13:41:16 +0800

    feat: Cat SVG

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,5 +1,13 @@
+import { Box, Typography, Stack } from '@mui/material';
+import catImg from '../images/home-cat.svg';
+
 const Home = () => {
-  return <h1>Home</h1>;
+  return (
+    <Stack direction='row' justifyContent='center' alignItems='center'>
+      <Typography variant='h1'>Home</Typography>
+      <img src={catImg} alt='' />
+    </Stack>
+  );
 };
 
 export default Home;
