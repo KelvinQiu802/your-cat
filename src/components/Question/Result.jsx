01db6ad86bf3d84261117ee9b01be02ebadb48c6
KelvinQiu <kelvinqiu802@outlook.com>
2022-08-23 22:15:22 +0800

    feat: Result page

--- src/components/Question/Result.jsx
+++ src/components/Question/Result.jsx
@@ -1,9 +1,15 @@
+import { Paper } from '@mui/material';
 import { getRandInt } from '../../utils/tools';
 
 const Result = ({ result }) => {
   const cat = result[getRandInt(0, result.length - 1)];
   console.log(cat);
-  return <h1>{cat.name}</h1>;
+  return (
+    <Paper
+      elevation={3}
+      sx={{ width: '1100px', height: '700px', p: 4, borderRadius: 5 }}
+    ></Paper>
+  );
 };
 
 export default Result;
