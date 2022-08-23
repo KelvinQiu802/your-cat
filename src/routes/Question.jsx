e5ecfe8c7509a362b0d142e4bbff76827461373b
KelvinQiu <kelvinqiu802@outlook.com>
2022-08-23 22:15:57 +0800

    style: refactor components

--- src/routes/Question.jsx
+++ src/routes/Question.jsx
@@ -1,14 +1,26 @@
 import { Box, useTheme, useMediaQuery } from '@mui/material';
 import QuestionCard from '../components/Question/QuestionCard';
+import React from 'react';
+import Result from '../components/Question/Result';
 
 const Question = () => {
   const theme = useTheme();
   const mobile = useMediaQuery(theme.breakpoints.down(''));
 
+  const [result, setResult] = React.useState(null);
+
   return (
-    <Box className={mobile ? 'wrapper-card' : 'wrapper-card'}>
-      <QuestionCard />
-    </Box>
+    <>
+      {result ? (
+        <Box className='wrapper-result'>
+          <Result result={result} />
+        </Box>
+      ) : (
+        <Box className={mobile ? 'wrapper-card' : 'wrapper-card'}>
+          <QuestionCard result={result} setResult={setResult} />
+        </Box>
+      )}
+    </>
   );
 };
 
