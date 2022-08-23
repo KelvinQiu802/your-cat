dfe266732be20767023d8ded09c90b9e6227bf63
KelvinQiu <kelvinqiu802@outlook.com>
2022-08-23 22:44:05 +0800

    feaet: cat image

--- src/components/Question/Result.jsx
+++ src/components/Question/Result.jsx
@@ -1,14 +1,27 @@
-import { Paper } from '@mui/material';
+import {
+  ImageList,
+  ImageListItem,
+  Paper,
+  Skeleton,
+  Stack,
+} from '@mui/material';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import { getRandInt } from '../../utils/tools';
 
 const Result = ({ result }) => {
   const cat = result[getRandInt(0, result.length - 1)];
+
   console.log(cat);
   return (
-    <Paper
-      elevation={3}
-      sx={{ width: '1100px', height: '700px', p: 4, borderRadius: 5 }}
-    ></Paper>
+    <Paper elevation={3} sx={{ width: '1100px', p: 4, borderRadius: 5 }}>
+      <Stack>
+        <Stack direction='row'>
+          <img src={cat.image.url} alt='' width='400px' />
+        </Stack>
+        {/* Below Information */}
+      </Stack>
+    </Paper>
   );
 };
 
