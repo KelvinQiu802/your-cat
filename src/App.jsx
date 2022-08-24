def3d6337de66c9e0807147b2cf4c43e44a216c2
KelvinQiu <kelvinqiu802@outlook.com>
2022-08-24 10:52:12 +0800

    feat: mobile nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,38 @@
 import './App.css';
 import { NavLink, Outlet } from 'react-router-dom';
+import { Pets } from '@mui/icons-material';
+import { useTheme, useMediaQuery } from '@mui/material';
 
 const App = () => {
+  const theme = useTheme();
+  const mobile = useMediaQuery(theme.breakpoints.down('md'));
+
   return (
     <>
-      <nav className='nav-bar'>
-        <NavLink to='/' className='nav-link'>
+      <nav className={mobile ? 'nav-bar-mobile' : 'nav-bar'}>
+        <Pets
+          color='warning'
+          sx={{
+            verticalAlign: 'middle',
+            position: 'relative',
+            bottom: '4px',
+            fontSize: '2em',
+          }}
+        />
+        <NavLink
+          to='/'
+          className={({ isActive }) =>
+            isActive ? 'link-selected' : 'nav-link'
+          }
+        >
           Home
         </NavLink>
-        <NavLink to='all' className='nav-link'>
+        <NavLink
+          to='all'
+          className={({ isActive }) =>
+            isActive ? 'link-selected' : 'nav-link'
+          }
+        >
           All Breeds
         </NavLink>
       </nav>
