d8a650e71789eaa2d0198b72a389668e6a557a3b
KelvinQiu <kelvinqiu802@outlook.com>
2022-08-24 11:27:41 +0800

    feat: add 404 page

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './routes/Home';
 import All from './routes/All';
 import Question from './routes/Question';
+import NotFound from './routes/NotFound';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -16,6 +17,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
           <Route index element={<Home />} />
           <Route path='all' element={<All />} />
           <Route path='question' element={<Question />} />
+          <Route path='*' element={<NotFound />} />
         </Route>
       </Routes>
     </BrowserRouter>
